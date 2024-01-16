88d50eeb51b4294e16894b8348d64143c9f21027
haiqin <3567840474@qq.com>
2024-01-16 15:53:12 +0800

    address access

--- instr.cpp
+++ instr.cpp
@@ -1,28 +1,155 @@
+
 #include "instr.h"
 #include "constant.h"
-#include <fstream>
-#include <sstream>
 #include <iostream>
-#include <qstring.h>
+
+namespace instr6502 {
+void BRK(Registers* registers, Bus* bus) {
+
+}
+
+void LD(Registers* registers, Bus* bus, char target, u8 value) {
+
+}
+
+void ST(Registers* registers, Bus* bus, char* target, u8 value) {
+
+}
+
+void STZ(Registers* registers, Bus* bus, char* target) {
+
+}
+}
+
+namespace access6052{
+
+u8 impliedAccess(Registers* registers, Bus* bus, int& cycle) {
+    cycle = 0;
+
+    return 0;
+}
+
+u8 immediateAccess(Registers* registers, Bus* bus, int& cycle) {
+    u16 addr = registers->PC;
+
+    cycle = 0;
+    return bus->read(addr);
+}
+
+u8 zeropageAccess(Registers* registers, Bus* bus, char regis, int& cycle) {
+    u16 addr = registers->PC;
+    u8 offset = bus->read(addr);
+    switch (regis) {
+    case 'X' : offset += registers->X;break;
+    case 'Y' : offset += registers->Y;break;
+    case '0' : break;
+    default: std::cout << "zero page access error: invalid register";break;
+    }
+
+    cycle = 0;
+
+    return bus->read(0x00ff & offset);
+}
+
+u8 absoluteAccess(Registers* registers, Bus* bus, char regis, int& cycle) {
+    u16 addr1 = registers->PC;
+    registers->PC++;
+    u8 low = bus->read(addr1);
+
+    u16 addr2 = registers->PC;
+    registers->PC++;
+    u8 high = bus->read(addr2);
+
+    u16 targetAddr = (((u16)(high)) << 8) & low;
+
+    switch (regis) {
+    case 'X' : targetAddr += registers->X; break;
+    case 'Y' : targetAddr += registers->Y; break;
+    case '0' : break;
+    default: std::cout << "zero page access error: invalid register";break;
+    }
+    cycle = ((targetAddr & 0xff00) >> 8) != high;
+
+    return bus->read(targetAddr);
+}
+
+u8 indirectAccess(Registers* registers, Bus* bus, int& cycle) {
+    u16 addr1 = registers->PC;
+    registers->PC++;
+    u8 low = bus->read(addr1);
+
+    u16 addr2 = registers->PC;
+    registers->PC++;
+    u8 high = bus->read(addr2);
+
+    u16 targetAddr = (((u16)(high)) << 8) & low;
+
+    low = bus->read(targetAddr);
+    high = bus->read(targetAddr + 1);
+    targetAddr = (((u16)(high)) << 8) & low;
+
+    cycle = 0;
+    return bus->read(targetAddr);
+}
+
+u8 indirectAccessX(Registers* registers, Bus* bus, int& cycle) {
+    u8 offset = bus->read(registers->PC) + registers->X;
+    registers->PC++;
+
+    u8 low = bus->read(0x00ff & offset);
+    u8 high = bus->read(0x00ff & (offset + 1));
+
+    u16 targetAddr = (((u16)(high)) << 8) & low;
+
+    cycle = 0;
+    return bus->read(targetAddr);
+}
+
+u8 indirectAccessY(Registers* registers, Bus* bus, int& cycle) {
+    u8 offset = bus->read(registers->PC);
+    registers->PC++;
+
+    u8 low = bus->read(0x00ff & offset);
+    u8 high = bus->read(0x00ff & (offset + 1));
+
+    u16 targetAddr = (((u16)(high)) << 8) & low;
+    targetAddr += registers->Y;
+    cycle = ((targetAddr & 0xff00) >> 8) != high;
+
+    return bus->read(targetAddr);
+}
+
+u8 relativeAccess(Registers* registers, Bus* bus, int& cycle) {
+
+    u16 offset = bus->read(registers->PC);
+    registers->PC++;
+    if(offset & (0x0080)) {
+        offset |= 0xff00;
+    }
+    registers->PC += offset;
+
+    cycle = 0;
+    return 0;
+}
+}
 
 Instr::Instr(Registers* registers, Bus* bus) : registers_(registers), bus_(bus)
 {
-    this->func_ = [](Registers* registers, Bus* bus) {
-        std::cout << "illegal instrction" << std::endl;
+    func_ = [](Registers* registers, Bus* bus) {
+        std::cout << "invalid instrction" << std::endl;
+        return 0;
     };
 }
 
-Instr::Instr(Registers* registers, Bus* bus, std::function<void(Registers*, Bus*)> func)
-    : registers_(registers), bus_(bus), func_(func)
-{
+Instr::Instr(Registers* registers, Bus* bus, std::function<void(Registers* registers, Bus* bus)> func) : registers_(registers), bus_(bus), func_(func) {
 
 }
 
-bool Instr::execute() {
-    this->func_(this->registers_, this->bus_);
+void Instr::execute() {
+    func_(this->registers_, this->bus_);
 }
 
-void InstrGenerator::init(const std::string& filePath, Registers* registers, Bus* bus) {
+void InstrGenerator::init(Registers* registers, Bus* bus) {
     instrTypeTable.resize(Constant::instrTableSize);
     cycleNumTable.resize(Constant::instrTableSize);
     table.resize(Constant::instrTableSize);
@@ -32,39 +159,18 @@ void InstrGenerator::init(const std::string& filePath, Registers* registers, Bus
         table[i].resize(Constant::instrTableSize);
     }
 
-    std::ifstream fs;
-    fs.open(filePath);
-    if(!fs.is_open()) {
-        std::cout << "fail to open " << filePath << std::endl;
-        return;
-    }
 
-    std::string line;
-    int i = 0;
-    while(std::getline(fs, line)) {
-        if(line[0] == '#') {
-            continue;
-        }
-        std::stringstream ss(line);
-        for(int j = 0; j < Constant::instrTableSize; j++) {
-            int tmp = 0;
-            ss >> tmp;
-            cycleNumTable[i][j] = tmp;
-        }
-        i++;
-        if(i == Constant::instrTableSize) {
-            break;
-        }
-    }
     for(int i = 0; i < Constant::instrTableSize; i++) {
         for(int j = 0; j < Constant::instrTableSize; i++) {
             
         }
     }
 
+    for(int i = 0; i < Constant::instrTableSize; i++) {
+        for(int j = 0; j < Constant::instrTableSize; j++) {
+            table[i][j] = new Instr(registers, bus, [](Registers* registers, Bus* bus) {
 
-    table[0][0] = new Instr(registers, bus, [](Registers* registers, Bus* bus) {
-        u8 opCode = bus->read(registers->PC);
-
-    });
+            });
+        }
+    }
 }
--- instr.h
+++ instr.h
@@ -3,7 +3,6 @@
 #include "registers.h"
 #include "bus.h"
 #include <vector>
-#include <string>
 #include <functional>
 
 class Instr
@@ -11,12 +10,20 @@ class Instr
 private:
     Registers* registers_;
     Bus* bus_;
-    std::function<void(Registers*, Bus*)> func_;
-public:
-    Instr(Registers* registers, Bus* bus);
-    Instr(Registers* registers, Bus* bus, std::function<void(Registers*, Bus*)> func);
+    std::function<void(Registers* registers, Bus* bus)> func_;
 
-    bool execute();
+public:
+
+    Instr(Registers* registers, Bus* bus);
+    Instr(Registers* registers, Bus* bus, std::function<void(Registers* registers, Bus* bus)> func);
+
+    void execute();
+
+    static void immediateAccess();
+    static void indirectAccess(Bus* bus);
+    static void absoluteAccess(Bus* bus);
+    static void zeropageAccess(Registers* registers, Bus* bus, char regis);
+    static void impliedAccess();
 };
 
 
@@ -27,16 +34,8 @@ public:
     static std::vector<std::vector<int>> instrTypeTable;
 
     static std::vector<std::vector<Instr*>> table;
-    static void init(const std::string& pathName, Registers* registers, Bus* bus);
+    static void init(Registers* registers, Bus* bus);
     static Instr* generateInstr(u8 high, u8 low);
-
-    static u8 accumulatorAccess(u16 PC);
-    static u8 ImpliedAccess(u16 PC);
-    static u8 absoluteAccess(u16 PC);
-    static u8 zeroPageAccess(u16 PC);
-    static u8 relativeAccess(u16 PC);
-    static u8 indirectAccess(u16 PC);
-    static u8 zeroPageIndexAccess(u16 PC);
 
 
 };
